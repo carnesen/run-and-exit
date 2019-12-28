b8316b53fb13178563496b785b465efbf24ae346
Chris Arnesen <chris.arnesen@gmail.com>
2019-12-28 07:04:58 -0800

    Update dependencies

--- example.js
+++ example.js
@@ -5,5 +5,6 @@ const { promisify } = require('util');
 
 runAndExit(async () => {
   const fileContents = await promisify(readFile)(__filename, 'utf8');
+  console.log("By default the return value is console.log'ed too");
   return fileContents;
 });
