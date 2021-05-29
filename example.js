361711540b24980932a48e4d37d95b4ed4730e47
Chris Arnesen <chris.arnesen@gmail.com>
2021-05-29 08:02:06 -0700

    Upgrade dependencies for marketing release 1.0

--- example.js
+++ example.js
@@ -5,6 +5,7 @@ const { runAndExit } = require('.');
 
 runAndExit(async () => {
 	const fileContents = await promisify(readFile)(__filename, 'utf8');
+	// eslint-disable-next-line no-console
 	console.log("By default the return value is console.log'ed too");
 	return fileContents;
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 /**
  * Run a function or async function, console.log the result, and process.exit
  * @param fn A function or async function
- * @param args Arguments to pass to the function. Must be suitably typed.
- * @typeParam TArgs Type of the arguments
+ * @param fnArgs Arguments to pass to the function. Must be suitably typed.
+ * @typeParam TArgs Type of the arguments. Inferred from fn.
  */
 export function runAndExit<TArgs extends any[]>(
 	fn: (...args: TArgs) => any,
-	...args: TArgs
+	...fnArgs: TArgs
 ): void {
 	(async () => {
 		try {
-			const value = await fn(...args);
+			const value = await fn(...fnArgs);
 			if (typeof value !== 'undefined') {
 				console.log(value); // eslint-disable-line no-console
 			}
